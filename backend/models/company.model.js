e9d7ee415784faae256abb69711ca83be3d9cb9d
mukti <muktimishra0495@gmail.com>
2026-02-21 01:53:07 +0530

    Made company schema

---
+++ backend/models/company.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    website: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    
+    logo: {
+        type: String,
+    },
+    company:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+        required: true
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    applications:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Application",
+            required: true
+        }
+    ]
+})
